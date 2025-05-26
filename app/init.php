327d2766b675f54333735dff3ea4072c08168950
WayanBerdyanto <aber.panak.atok@gmail.com>
2025-05-26 15:05:38 +0700

    initial commit

---
+++ app/init.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'core/App.php';
+require_once 'core/Controller.php';
+
+//Menambahkan sebuah konstanta
+require_once 'config/Config.php';
+
+//Panggil Database
+require_once 'core/Database.php';
+
+//Menjalankan flasher
+// require_once 'core/Flasher.php';
+?>
